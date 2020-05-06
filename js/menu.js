bd280808c5e81692da2ecfeb2578939dbcbcb749
nick-yushenko <59775649+nick-yushenko@users.noreply.github.com>
2020-05-06 14:37:52 +0700

    interface + его адаптив

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,28 @@
 (function () {
 
   const menu = document.querySelector('.menu')
+  const menuItems = document.querySelectorAll('.menu-item__text')
   const menuTrigger = document.querySelector('.js-menuTrigger')
   let isOpen = false
 
   menuTrigger.addEventListener('click', function () {
     if (!isOpen) {
-      menu.style.width = '340px'
+      if (window.innerWidth <= 425)
+        menu.style.width = '100%'
+      else
+        menu.style.width = '340px'
+      menuItems.forEach(function (item) {
+        // item.style.display = 'block'
+      })
       isOpen = true
     } else {
-      menu.style.width = '90px'
+      if (window.innerWidth <= 1024)
+        menu.style.width = '80px'
+      else
+        menu.style.width = '96px'
+      menuItems.forEach(function (item) {
+        // item.style.display = 'none'
+      })
       // menu.style.transform = 'translateX(-340px)'
       isOpen = false
 
